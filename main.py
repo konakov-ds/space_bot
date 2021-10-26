6dcd417676c3df454cd103af7b2ae788fc5bd7d3
Mark Konakov <konakovmark@yandex.ru>
2021-10-26 09:25:44 +0300

    Create load_img func

--- main.py
+++ main.py
@@ -1,8 +1,25 @@
 import requests
+import os
 
 url = 'https://upload.wikimedia.org/wikipedia/commons/3/3f/HST-SM4.jpeg'
-response = requests.get(url)
-response.raise_for_status()
+dir_path = 'images/'
+img_name = 'hubble.jpeg'
+os.makedirs(dir_path, exist_ok=True)
 
-with open('hubble.jpeg', 'wb') as img:
-    img.write(response.content)
+
+def load_img(url, name):
+    img_path = os.path.join(dir_path, name)
+
+    response = requests.get(url)
+    response.raise_for_status()
+
+    with open(img_path, 'wb') as img:
+        img.write(response.content)
+
+    dir_content = os.listdir(dir_path)
+    if name in dir_content:
+        print('Image load successfully!')
+
+
+if __name__ == '__main__':
+    load_img(url, img_name)
