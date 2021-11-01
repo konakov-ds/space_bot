a7f9a3e802ccf2de67cee580bf87a04ac9415f23
Mark Konakov <konakovmark@yandex.ru>
2021-11-01 09:52:39 +0300

    Add dir_path to send_photo func

--- main.py
+++ main.py
@@ -5,12 +5,12 @@ import telegram
 from dotenv import load_dotenv
 
 
-def send_photo_to_channel(delay, channel, token):
+def send_photo_to_channel(delay, channel, token, dir_path):
     bot = telegram.Bot(token=token)
-    images = os.listdir('images/')
+    images = os.listdir(dir_path)
     while True:
         for image in images:
-            img_path = os.path.join('images', image)
+            img_path = os.path.join(dir_path, image)
             with open(img_path, 'rb') as p:
                 bot.send_photo(
                     chat_id=channel,
@@ -24,5 +24,6 @@ if __name__ == '__main__':
     token = os.getenv('TELEGRAM_TOKEN')
     delay = float(os.getenv('TIME_DELAY'))
     channel = os.getenv('TG_CHANNEL')
+    dir_path = 'images'
 
-    send_photo_to_channel(delay, channel, token)
+    send_photo_to_channel(delay, channel, token, dir_path)
